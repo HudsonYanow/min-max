35d7beb076039e758b3954b669a40891f7f214ab
HudsonYanow <yanowhudson@gmail.com>
2024-11-21 17:56:49 -0800

    doc+menu

--- minmax.py
+++ minmax.py
@@ -1,42 +1,63 @@
 class MinMax:
+    """
+    A class to find the minimum and maximum values, as well as ranges of these values, 
+    in a given list of numbers.
+
+    Attributes:
+        arr (list): The list of numbers to process.
+    """
+
     def __init__(self, arr):
-        self.arr=arr
+        """
+        Initializes the MinMax object with a list of numbers.
+
+        Args:
+            arr (list): A list of numeric values.
+        """
+        self.arr = arr
+
     def getMin(self):
-        min=self.arr[0]
+        """
+        Finds the smallest value in the list.
+
+        Returns:
+            int/float: The smallest number in the list.
+        """
+        min_val = self.arr[0]
         for num in self.arr:
-            if num<min:
-                min=num
-        return min
+            if num < min_val:
+                min_val = num
+        return min_val
+
     def getMinRange(self, n):
-        smallNum=[]
-        carr=self.arr.copy()
-        for i in range(n):
-            min=carr[0]
+        """
+        Finds the `n` smallest values in the list.
+
+        Args:
+            n (int): The number of smallest values to find.
+
+        Returns:
+            list: A list of the `n` smallest numbers in ascending order.
+        """
+        smallNum = []
+        carr = self.arr.copy()
+        for _ in range(n):
+            min_val = carr[0]
             for num in carr:
-                if num<min:
-                    min=num
-            smallNum.append(min)
-            carr.remove(min)
+                if num < min_val:
+                    min_val = num
+            smallNum.append(min_val)
+            carr.remove(min_val)
         return smallNum
+
     def getMax(self):
-            max=self.arr[0]
-            for num in self.arr:
-                if num>max:
-                    max=num
-            return max
-    def getMaxRange(self, n):
-        bigNum=[]
-        carr=self.arr.copy()
-        for i in range(n):
-            max=carr[0]
-            for num in carr:
-                if num>max:
-                    max=num
-            bigNum.append(max)
-            carr.remove(max)
-        return bigNum      
-min=MinMax([3,7,20,15,2])
-print(min.getMin())
-print(min.getMinRange(3)) 
-print(min.getMax())
-print(min.getMaxRange(3))
+        """
+        Finds the largest value in the list.
+
+        Returns:
+            int/float: The largest number in the list.
+        """
+        max_val = self.arr[0]
+        for num in self.arr:
+            if num > max_val:
+                max_val =
